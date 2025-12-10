38c89563b64be3956dd817812f7347e6c93436cb
MarionBurguin <marion91.burguin@gmail.com>
2025-12-10 12:03:34 +0100

    Update concours_photo.php
    changement chemin css

--- concours_photo.php
+++ concours_photo.php
@@ -89,7 +89,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit_vote'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Concours Photo - Soumission et Vote</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="CSS_CREA/style.css">
 </head>
 <body>
     
